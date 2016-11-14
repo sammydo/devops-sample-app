1ea1803be2284b662c287c2c070ddb7b3b87ca93
Stephen Reid <steveyblam@hotmail.co.uk>
2016-11-14 14:57:05 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine' , 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/' , function(req , res){
+
+  res.render("index");
+
+});
+
+app.listen(3000 , function(){
+
+  console.log('Your app is ready and listening on port 3000');
+
+});
