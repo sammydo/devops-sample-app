f10f25f6e460cf1a7589d210e5f4f9ada81358d8
Stephen Reid <steveyblam@hotmail.co.uk>
2016-12-15 11:48:12 +0000

    added fibonacci route for load testing

--- app.js
+++ app.js
@@ -11,10 +11,32 @@ app.get('/' , function(req , res){
 
 });
 
+app.get('/fibonacci/:n' , function(req,res){
+
+  // high cpu usage function
+  var answer = fibonacci(req.params.n);
+
+  res.json(answer);
+
+});
+
 app.listen(3000 , function(){
 
   console.log('Your app is ready and listening on port 3000');
 
 });
+
+// deliberately poorly implemented fibonnaci
+function fibonacci(n) {
+
+  if(n == 0)
+    return 0;
+
+  if(n == 1)
+    return 1;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+
+}
 
 module.exports = app;
